f3863532e77b8508b645c505f53a3ac7052ba624
Gerhard Kurz <gerhard.kurz@kit.edu>
2015-09-01 15:08:17 +0200

    * added parameters from paper as variables to allow changing the accuracy * use 12 digit accuracy

--- lib/external/circVMcdf.m
+++ lib/external/circVMcdf.m
@@ -16,58 +16,75 @@ function res = circVMcdf(T,VK)
 %  (A BibTeX citation is in a comment at the end of this file)
 %
 %  By Shai Revzen, Berkeley 2006
+%  Modified by Gerhard Kurz, KIT 2015
 
-  if length(VK) ~= 1
-    error('circ:mustBeScalar','kurtosis must be a scalar')
-  end
-  %T = T-mu;
-  Z = VK;
-  U = mod(T+pi,2*pi);
-  Y = U-pi;
-  if Z>10.5
-    res = largeVK(Y, Z);
-  elseif Z<=0
-    res = (U*0.5)/pi;
-  else
-    V = smallVK(Y, Z);
-    res = (U*0.5+V)/pi;
-  end
-  res(res<0)=0;
-  res(res>1)=1;
+
+    % 8 digit accuracy
+    % CK = 10.5;
+    % 12 digit accuracy
+    CK = 50;
+
+    if length(VK) ~= 1
+        error('circ:mustBeScalar','kurtosis must be a scalar')
+    end
+    %T = T-mu;
+    Z = VK;
+    U = mod(T+pi,2*pi);
+    Y = U-pi;
+    if Z>CK
+        res = largeVK(Y, Z);
+    elseif Z<=0
+        res = (U*0.5)/pi;
+    else
+        V = smallVK(Y, Z);
+        res = (U*0.5+V)/pi;
+    end
+    res(res<0)=0;
+    res(res>1)=1;
 end
 
-function V = smallVK( Y, Z )      
-  IP = Z*0.8 - 8.0/(Z+1.0) + 12;
-  P = round(IP);
-  S = sin(Y);
-  C = cos(Y);
-  Y = P*Y;
-  SN = sin(Y);
-  CN = cos(Y);
-  R = 0.0;
-  V = 0.0;
-  Z = 2.0/Z;
-  for N=2:round(IP)
-    P = P - 1.0;
-    Y = SN;
-    SN = SN.*C - CN.*S;
-    CN = CN.*C + Y.*S;
-    R = 1.0./(P*Z+R);
-    V = (SN./P+V)*R;
-  end
+function V = smallVK( Y, Z )
+    % 8 digit accuracy
+    %A1 = 12; A2 = 0.8; A3 = 8.0; A4 = 1.0;   
+    % 12 digit accuracy    
+    A1 = 28; A2 = 0.5; A3 = 100.0; A4 = 5.0;   
+    
+    IP = Z*A2 - A3/(Z+A4) + A1;
+    P = round(IP);
+    S = sin(Y);
+    C = cos(Y);
+    Y = P*Y;
+    SN = sin(Y);
+    CN = cos(Y);
+    R = 0.0;
+    V = 0.0;
+    Z = 2.0/Z;
+    for N=2:round(IP)
+        P = P - 1.0;
+        Y = SN;
+        SN = SN.*C - CN.*S;
+        CN = CN.*C + Y.*S;
+        R = 1.0./(P*Z+R);
+        V = (SN./P+V)*R;
+    end
 end
 
-function res = largeVK( Y, Z )      
-  C = 24.0 * Z;
-  V = C - 56;
-  R=sqrt((54.0/(347.0/V+26.0-C)-6.0+C)/12.0);
-  Z=sin(Y*0.5)*R;
-  S=Z.*Z*2.0;                                           
-  V = V - S + 3.0;
-  Y = (C-S-S-16.0)/3.0;
-  Y = ((S+1.75)*S+83.5)/V - Y;
-  res=erf(Z-S/(Y.*Y).*Z)*0.5+0.5;
-end  
+function res = largeVK( Y, Z )
+    % 8 digit accuracy
+    % C1 = 56;
+    % 12 digit accuracy
+    C1 = 50.1;
+
+    C = 24.0 * Z;
+    V = C - C1;
+    R=sqrt((54.0/(347.0/V+26.0-C)-6.0+C)/12.0);
+    Z=sin(Y*0.5)*R;
+    S=Z.*Z*2.0;
+    V = V - S + 3.0;
+    Y = (C-S-S-16.0)/3.0;
+    Y = ((S+1.75)*S+83.5)/V - Y;
+    res=erf(Z-S/(Y.*Y).*Z)*0.5+0.5;
+end
 
 % BibTeX:
 %  @article{355753,
