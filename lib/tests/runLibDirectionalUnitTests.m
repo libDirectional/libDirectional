6fb375a5b320c442bf7c7b6bc1c32f81fb37e2f8
Gerhard Kurz <gerhard.kurz@kit.edu>
2015-12-02 17:27:04 +0100

    renamed runUnitTests to avoid conflicts with the nonlinear estimation toolbox

--- lib/tests/runLibDirectionalUnitTests.m
+++ lib/tests/runLibDirectionalUnitTests.m
@@ -1,10 +1,13 @@
-function results = runUnitTests(enableExpensiveTests)
+function results = runLibDirectionalUnitTests(enableExpensiveTests)
     global enableExpensive
     if nargin>0
         enableExpensive=enableExpensiveTests;
     else
         enableExpensive=false;
     end
+    
+    warning on %#ok<WNON> % enable all warnings in case they were previously disabled
+    
     % Execute all unit tests.
     
     % switch to path of this file
