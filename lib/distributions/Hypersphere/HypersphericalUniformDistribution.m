e7b21d2ed02fd5ef38d92f46a2f61bf52913c099
Gerhard Kurz <gerhard.kurz@kit.edu>
2015-12-20 04:15:14 +0100

    tests, assertions, comments

--- lib/distributions/Hypersphere/HypersphericalUniformDistribution.m
+++ lib/distributions/Hypersphere/HypersphericalUniformDistribution.m
@@ -1,28 +1,51 @@
 classdef HypersphericalUniformDistribution < AbstractHypersphericalDistribution
     methods
-        function this=HypersphericalUniformDistribution(dim_)
-            this.dim=dim_;
+        function this = HypersphericalUniformDistribution(dim_)
+            assert(dim_>=2)
+            this.dim = dim_;
         end
         
-        function vals=pdf(this,xa)
+        function vals = pdf(this, xa)
+            % Evaluates pdf at each column of xa
+            % Parameters:
+            %   xa (d x n matrix)
+            %       each column represents one of the n points in R^d that the
+            %       pdf is evaluated at; can be just a (d x 1) vector as well
+            % Returns:
+            %   p (1 x n row vector)
+            %       values of the pdf at each column of xa
+            
             % pdf is always 1 divided by surface area (integrates to 1).
             vals = (1/AbstractHypersphericalDistribution.computeUnitSphereSurface(this.dim))*ones(1,size(xa,2));
         end
         
-        function X=sample(this,n)
+        function s = sample(this, n)
+            % Stocahastic sampling
+            %
+            % Parameters:
+            %   n (scalar)
+            %       number of samples
+            % Returns:
+            %   s (dim x n)
+            %       one sample per column     
+            %
             % General algorithm based on "A note on a method for generating 
             % points uniformly on n-dimensional spheres" by Mervin E. Muller April 1959.
             % Algorithm for 2-sphere based on "Spherical sampling by archimedes' theorem"
             % by Min-Zhi Shao and Norman Badler, 1996
-            if this.d==3
-                X=NaN(this.dim,n);
-                phi=2*pi*rand(1,n);
-                X(3,:)=rand(n,1)*2-1;
-                r=sqrt(1-X(3,:).^2);
-                X(1,:)=r.*cos(phi);
-                X(2,:)=r.*sin(phi);
+
+            assert(isscalar(n));
+            assert(n>0);
+            
+            if this.dim == 3
+                s = NaN(this.dim,n);
+                phi = 2*pi*rand(1,n);
+                s(3,:) = rand(n,1)*2-1;
+                r = sqrt(1-s(3,:).^2);
+                s(1,:) = r.*cos(phi);
+                s(2,:) = r.*sin(phi);
             else
-                X=cell2mat(cellfun(@(x){x/norm(x)},num2cell(randn(this.dim,n),1)));
+                s=cell2mat(cellfun(@(x){x/norm(x)},num2cell(randn(this.dim,n),1)));
             end
         end
     end 
--- lib/tests/distributions/HypersphericalUniformDistributionTest.m
+++ lib/tests/distributions/HypersphericalUniformDistributionTest.m
@@ -1,19 +1,29 @@
 classdef HypersphericalUniformDistributionTest< matlab.unittest.TestCase    
     methods (Test)
         function testIntegral(testCase)
-            for dim=1:4
-                hud=HypersphericalUniformDistribution(dim);
+            for dim = 2:4
+                hud = HypersphericalUniformDistribution(dim);
                 testCase.verifyEqual(hud.integral,1,'AbsTol',1E-6);
             end
         end
         
         function testPdf(testCase)
             rng default
-            for dim=1:4
-                hud=HypersphericalUniformDistribution(dim);
+            for dim = 2:4
+                hud = HypersphericalUniformDistribution(dim);
                 x = rand(dim,1);
                 x = x/norm(x);
                 testCase.verifyEqual(hud.pdf(x),1/AbstractHypersphericalDistribution.computeUnitSphereSurface(dim),'AbsTol',1E-10);
+            end
+        end
+        
+        function testSample(testCase)
+            for dim = 2:4
+                hud = HypersphericalUniformDistribution(dim);
+                n = 10;
+                samples = hud.sample(n);
+                testCase.verifySize(samples, [hud.dim, n]);
+                testCase.verifyEqual(sum(samples.*samples), ones(1,n), 'RelTol', 1E-10);
             end
         end
     end
