2e38d02ece799acc6eefd58a6b61a83e6c23cf89
Gerhard Kurz <gerhard.kurz@kit.edu>
2016-03-08 13:14:54 +0100

    create TAP file when running unit tests

--- lib/tests/runLibDirectionalUnitTests.m
+++ lib/tests/runLibDirectionalUnitTests.m
@@ -15,8 +15,18 @@ function results = runLibDirectionalUnitTests(enableExpensiveTests)
     
     [~,dirName]=fileparts(pwd);
     if isequal(dirName,'tests')
-        results = runtests(pwd(), 'Recursively', true);
+        suite = matlab.unittest.TestSuite.fromFolder(pwd, matlab.unittest.internal.selectors.NeverFilterSelector, 'IncludingSubfolders', true);
+
+        runner = matlab.unittest.TestRunner.withTextOutput();
+        tapFile = fullfile(pwd, '../../testResults.tap');
+        fprintf('writing tap file: %s\n', tapFile);
+        if exist(tapFile, 'file')
+            delete(tapFile)
+        end
+        runner.addPlugin(matlab.unittest.plugins.TAPPlugin.producingOriginalFormat(matlab.unittest.plugins.ToFile(tapFile)));
+        results = runner.run(suite);
     else
         error('This command has to be run within the test directory in libDirectional.');
     end
 end
+
