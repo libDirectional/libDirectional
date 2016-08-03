aea556a1d07063d4feba76cc9aa6d1cc8725ae5f
Gerhard Kurz <gerhard.kurz@kit.edu>
2016-08-03 15:54:43 +0200

    comments implemented entropy

--- lib/distributions/Circle/PWCDistribution.m
+++ lib/distributions/Circle/PWCDistribution.m
@@ -1,15 +1,25 @@
 classdef PWCDistribution < AbstractCircularDistribution
-    % Piecewise constant (i.e. discrete) distribution
+    % Piecewise constant (i.e. discrete) distribution, similar to a
+    % histogram.
+    %
+    % Gerhard Kurz, Florian Pfaff, Uwe D. Hanebeck,
+    % Discrete Recursive Bayesian Filtering on Intervals and the Unit Circle
+    % Proceedings of the 2016 IEEE International Conference on Multisensor Fusion and Integration for Intelligent Systems (MFI 2016), 
+    % Baden-Baden, Germany, September 2016.
     
     properties
-        w %weights as row vector
+        w % weights as row vector
     end
     
     methods
         function this = PWCDistribution(w_)
             % Constructor
+            %
+            % Parameters:
+            %   w (1 x n)
+            %       weight for each interval            
             assert(size(w_,1)==1);
-            this.w = w_/(mean(w_)*2*pi);
+            this.w = w_/(mean(w_)*2*pi); % normalize automatically
         end
         
         function p = pdf(this, xa)
@@ -46,11 +56,30 @@ classdef PWCDistribution < AbstractCircularDistribution
             m = -1i/n*sum(interv);
         end
         
+        function e = entropy(this)
+            % Calculates the entropy analytically
+            %
+            % Returns:
+            %   e (scalar)
+            %       entropy of the distribution            
+            e = -2*pi/length(this.w) * sum(this.w .* log(this.w));
+        end
     end
         
     methods (Static)
         function w = calculateParametersNumerically(pdf, n)
             % Calculates the weights from a pdf
+            %
+            % Parameters:
+            %   pdf (function handle)
+            %       pdf of a circular density
+            %   n (scalar)
+            %       number of discretization steps
+            % Returns:
+            %   w (1 x n)
+            %       weights of the corresponding PWC
+            assert(isa(pdf, 'function_handle'));
+            assert(n>=1);
             w = zeros(1,n);
             for j=1:n
                 l = PWCDistribution.leftBorder(j,n);
@@ -61,18 +90,45 @@ classdef PWCDistribution < AbstractCircularDistribution
         
         function l = leftBorder(m, n)
             % Calculates the left border of the m-th interval for a total of n intervals
+            %
+            % Parameters:
+            %   n (scalar)
+            %       total number of intervals
+            %   m (scalar)
+            %       number of intervals whose border is to be computed
+            % Returns:
+            %   m (scalar)
+            %       left border of the m-th interval
             assert(1 <= m && m<=n);
             l = 2*pi/n*(m-1);
         end
         
         function r = rightBorder(m, n)
             % Calculates the right border of the m-th interval for a total of n intervals
+            %
+            % Parameters:
+            %   n (scalar)
+            %       total number of intervals
+            %   m (scalar)
+            %       number of intervals whose border is to be computed
+            % Returns:
+            %   m (scalar)
+            %       right border of the m-th interval            
             assert(1 <= m && m<=n);
             r = 2*pi/n*m;
         end
         
         function c = intervalCenter(m, n)
             % Calculates the center of the m-th interval for a total of n intervals
+            %
+            % Parameters:
+            %   n (scalar)
+            %       total number of intervals
+            %   m (scalar)
+            %       number of intervals whose center is to be computed
+            % Returns:
+            %   m (scalar)
+            %       center of the m-th interval            
             assert(1 <= m && m<=n);
             c = 2*pi/n*(m-0.5);
         end
