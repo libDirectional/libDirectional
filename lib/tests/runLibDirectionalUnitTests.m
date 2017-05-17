43f872a9370a81c97e8b1abc01b1ae38a007f9bd
Gerhard Kurz <gerhard.kurz@kit.edu>
2017-05-17 10:20:39 +0200

    minor simplification

--- lib/tests/runLibDirectionalUnitTests.m
+++ lib/tests/runLibDirectionalUnitTests.m
@@ -1,4 +1,6 @@
 function results = runLibDirectionalUnitTests(enableExpensiveTests)
+    % Execute all unit tests.
+
     global enableExpensive
     if nargin>0
         enableExpensive=enableExpensiveTests;
@@ -6,16 +8,14 @@ function results = runLibDirectionalUnitTests(enableExpensiveTests)
         enableExpensive=false;
     end
     
-    warning on %#ok<WNON> % enable all warnings in case they were previously disabled
-    
-    % Execute all unit tests.
-    
+    warning on % enable all warnings in case they were previously disabled
+        
     % switch to path of this file
     cd(fileparts(mfilename('fullpath')));
     
     [~,dirName]=fileparts(pwd);
     if isequal(dirName,'tests')
-        suite = matlab.unittest.TestSuite.fromFolder(pwd, matlab.unittest.internal.selectors.NeverFilterSelector, 'IncludingSubfolders', true);
+        suite = matlab.unittest.TestSuite.fromFolder(pwd, 'IncludingSubfolders', true);
 
         runner = matlab.unittest.TestRunner.withTextOutput();
         tapFile = fullfile(pwd, '../../testResults.tap');
@@ -29,4 +29,3 @@ function results = runLibDirectionalUnitTests(enableExpensiveTests)
         error('This command has to be run within the test directory in libDirectional.');
     end
 end
-
